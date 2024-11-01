a62431cd09b7c18cfb448c31f09b4161b1edac44
Essential Randomness <essentialrandomn3ss@gmail.com>
2024-10-31 17:13:13 -0700

    add ability to make posts and even add tags

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ const FIREBASE_CLIENT_CONFIG = {
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
 };
+const POSTING_BOARD_ID = "4b30fb7c-2aca-4333-aa56-ae8623a92b65";
 
 const firebaseAdminApp = admin.initializeApp({
   credential: admin.credential.cert("./service-account-key.json"),
@@ -30,14 +31,39 @@ const userCredentials = await signInWithCustomToken(
 );
 const userToken = await userCredentials.user.getIdToken(true);
 
-const allBoardsResponse = await fetch(
-  new URL(`/realms/slug/${REALM_NAME}`, process.env.BOBABOARD_SERVER_URL),
+// const allBoardsResponse = await fetch(
+//   new URL(`/realms/slug/${REALM_NAME}`, process.env.BOBABOARD_SERVER_URL),
+//   {
+//     headers: {
+//       Authorization: userToken,
+//       "Content-Type": "application/json",
+//     },
+//   }
+// );
+
+const postingResponse = await fetch(
+  new URL(`/boards/${POSTING_BOARD_ID}`, process.env.BOBABOARD_SERVER_URL),
   {
+    method: "POST",
     headers: {
       Authorization: userToken,
       "Content-Type": "application/json",
     },
+    body: JSON.stringify({
+      content: JSON.stringify([
+        {
+          insert: {
+            "oembed-embed": {
+              url: "https://nodejs.org/api/url.html",
+            },
+          },
+        },
+      ]),
+      whisperTags: ["the NodeJS documentation is hard to parse sometimes"],
+      categoryTags: ["programming", "documentation"],
+      contentWarnings: ["programming jargon"],
+    }),
   }
 );
 
-console.log(await allBoardsResponse.json());
+console.log(await postingResponse.json());
