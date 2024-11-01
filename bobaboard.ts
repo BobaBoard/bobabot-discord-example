a106753633c01e1e76c9f149b2a869ba493ed98d
Essential Randomness <essentialrandomn3ss@gmail.com>
2024-10-31 17:44:30 -0700

    code cleanup

---
+++ bobaboard.ts
@@ -0,0 +1,85 @@
+import admin from "firebase-admin";
+import { getAuth, signInWithCustomToken } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const FIREBASE_CLIENT_CONFIG = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+};
+
+let userAuthToken: string;
+const getFirebaseAuthToken = async () => {
+  if (userAuthToken) {
+    // TODO: This needs logic for when the token is here but is expired
+    return userAuthToken;
+  }
+  const firebaseAdminApp = admin.initializeApp({
+    credential: admin.credential.cert("./service-account-key.json"),
+  });
+  const firebaseClientApp = initializeApp(FIREBASE_CLIENT_CONFIG);
+  const firebaseClientAuth = getAuth(firebaseClientApp);
+
+  const userRecord = await firebaseAdminApp
+    .auth()
+    .getUserByEmail("contacts@fujocoded.com");
+  const customToken = await firebaseAdminApp
+    .auth()
+    .createCustomToken(userRecord.uid);
+  const userCredentials = await signInWithCustomToken(
+    firebaseClientAuth,
+    customToken
+  );
+
+  userAuthToken = await userCredentials.user.getIdToken(true);
+  return userAuthToken;
+};
+
+export const postUrlToBoard = async (params: {
+  boardId: string;
+  url: string;
+  // tags...
+}) => {
+  const postingResponse = await fetch(
+    new URL(`/boards/${params.boardId}`, process.env.BOBABOARD_SERVER_URL),
+    {
+      method: "POST",
+      headers: {
+        Authorization: await getFirebaseAuthToken()!,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: JSON.stringify([
+          {
+            insert: {
+              "oembed-embed": {
+                url: params.url,
+              },
+            },
+          },
+        ]),
+        // whisperTags: ["This was posted in response to a Discord message"],
+        // categoryTags: ["programming", "documentation"],
+        // contentWarnings: ["programming jargon"],
+      }),
+    }
+  );
+
+  return await postingResponse.json();
+};
+
+export const getRealmBoards = async (params: { realmSlug: string }) => {
+  const allBoardsResponse = await fetch(
+    new URL(
+      `/realms/slug/${params.realmSlug}`,
+      process.env.BOBABOARD_SERVER_URL
+    ),
+    {
+      headers: {
+        Authorization: await getFirebaseAuthToken()!,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  console.log(await allBoardsResponse.json());
+};
--- index.ts
+++ index.ts
@@ -1,37 +1,15 @@
 import dotenv from "dotenv";
-import admin from "firebase-admin";
-import { getAuth, signInWithCustomToken } from "firebase/auth";
-import { initializeApp } from "firebase/app";
+import { getRealmBoards, postUrlToBoard } from "./bobaboard";
 import { Client, Events, GatewayIntentBits, Message } from "discord.js";
 
-dotenv.config();
-
-const REALM_NAME = "twisted-minds";
 const POSTING_BOARD_ID = "4b30fb7c-2aca-4333-aa56-ae8623a92b65";
 const DISCORD_CHANNEL_NAME = "bot";
 
-const FIREBASE_CLIENT_CONFIG = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-};
-const firebaseAdminApp = admin.initializeApp({
-  credential: admin.credential.cert("./service-account-key.json"),
-});
-const firebaseClientApp = initializeApp(FIREBASE_CLIENT_CONFIG);
-const firebaseClientAuth = getAuth(firebaseClientApp);
+dotenv.config();
 
-const userRecord = await firebaseAdminApp
-  .auth()
-  .getUserByEmail("contacts@fujocoded.com");
-const customToken = await firebaseAdminApp
-  .auth()
-  .createCustomToken(userRecord.uid);
-const userCredentials = await signInWithCustomToken(
-  firebaseClientAuth,
-  customToken
-);
-const userToken = await userCredentials.user.getIdToken(true);
+// Uncommonent this API call to see what the IDs of the
+// boards in your realm are.
+// await getRealmBoards({ realmSlug: "twisted-minds" });
 
 // Create Discord client
 const client = new Client({
@@ -58,40 +36,10 @@ client.on(Events.MessageCreate, async (message) => {
 
   console.log(`Received message: ${message.content}`);
 
-  const postingResponse = await fetch(
-    new URL(`/boards/${POSTING_BOARD_ID}`, process.env.BOBABOARD_SERVER_URL),
-    {
-      method: "POST",
-      headers: {
-        Authorization: userToken,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        content: JSON.stringify([
-          {
-            insert: {
-              "oembed-embed": {
-                url: "https://nodejs.org/api/url.html",
-              },
-            },
-          },
-        ]),
-        whisperTags: ["This was posted in response to a Discord message"],
-        categoryTags: ["programming", "documentation"],
-        contentWarnings: ["programming jargon"],
-      }),
-    }
+  console.log(
+    await postUrlToBoard({
+      boardId: POSTING_BOARD_ID,
+      url: "https://www.bobaboard.com",
+    })
   );
-
-  console.log(await postingResponse.json());
 });
-
-// const allBoardsResponse = await fetch(
-//   new URL(`/realms/slug/${REALM_NAME}`, process.env.BOBABOARD_SERVER_URL),
-//   {
-//     headers: {
-//       Authorization: userToken,
-//       "Content-Type": "application/json",
-//     },
-//   }
-// );
